09761d769f5342e6816b7df5d67bf4da78f10ba2
scarfacedotcom <peteradaaku@gmail.com>
2023-07-11 05:50:47 +0100

    array mod

--- demo/demo.go
+++ demo/demo.go
@@ -2,6 +2,6 @@ package demo
 
 const NewValue = "changedValue"
 
-func UpdateArray1(array [2]string) {
+func UpdateArray1(array *[2]string) {
 	array[0] = NewValue
 }
--- demo/demo_test.go
+++ demo/demo_test.go
@@ -8,6 +8,6 @@ import (
 
 func TestUpdateArray(t *testing.T) {
 	testArray := [2]string{"Value1", "Value2"}
-	UpdateArray1(testArray)
+	UpdateArray1(&testArray)
 	assert.Equal(t, NewValue, testArray[0])
 }
